0466812bf6319c1bc10c323579ed7a504c6985de
vv15 <vv15@illinois.edu>
2023-08-10 15:06:57 -0500

    Copied over L2 RTL files from esp-caches

---
+++ l2_spandex/rtl/l2_bufs.sv
@@ -0,0 +1,71 @@
+// Copyright (c) 2011-2022 Columbia University, System Level Design Group
+// SPDC-License-Identifier: Apache-2.0
+
+`timescale 1ps / 1ps
+`include "cache_consts.svh"
+`include "cache_types.svh"
+
+module l2_bufs(
+    input logic clk,
+    input logic rst, 
+    input logic rd_mem_en,
+    input l2_way_t way,
+    input var line_t rd_data_line[`L2_WAYS],
+    input var l2_tag_t rd_data_tag[`L2_WAYS],
+    input var hprot_t rd_data_hprot[`L2_WAYS],
+    input l2_way_t rd_data_evict_way, 
+    input var state_t rd_data_state[`L2_WAYS],
+
+    output l2_way_t evict_way_buf, 
+    output line_t lines_buf[`L2_WAYS],
+    output l2_tag_t tags_buf[`L2_WAYS],
+    output hprot_t hprots_buf[`L2_WAYS],
+    output state_t states_buf[`L2_WAYS]
+    );
+    
+    always_ff @(posedge clk or negedge rst) begin 
+        if (!rst) begin 
+            evict_way_buf <= 0; 
+        end else if (rd_mem_en) begin 
+            evict_way_buf <= rd_data_evict_way;
+        end
+    end
+
+    genvar i;
+    generate
+        for (i = 0; i < `L2_WAYS; i++) begin 
+            always_ff @(posedge clk or negedge rst) begin 
+                if (!rst) begin
+                    lines_buf[i] <= 0; 
+                end else if (rd_mem_en) begin 
+                    lines_buf[i] <= rd_data_line[i];
+                end
+            end
+
+            always_ff @(posedge clk or negedge rst) begin
+                if (!rst) begin 
+                    tags_buf[i] <= 0;
+                end else if (rd_mem_en) begin  
+                    tags_buf[i] <= rd_data_tag[i]; 
+                end
+            end
+
+            always_ff @(posedge clk or negedge rst) begin        
+                if (!rst) begin 
+                    hprots_buf[i] <= 0;
+                end else if (rd_mem_en) begin
+                    hprots_buf[i] <= rd_data_hprot[i]; 
+                end
+            end
+
+            always_ff @(posedge clk or negedge rst) begin
+                if (!rst) begin 
+                    states_buf[i] <= 0;
+                end else if (rd_mem_en) begin
+                    states_buf[i] <= rd_data_state[i]; 
+                end
+            end
+        end
+    endgenerate
+
+endmodule
---
+++ l2_spandex/rtl/l2_lookup.sv
@@ -0,0 +1,77 @@
+// Copyright (c) 2011-2022 Columbia University, System Level Design Group
+// SPDC-License-Identifier: Apache-2.0
+
+`timescale 1ps / 1ps
+`include "cache_consts.svh"
+`include "cache_types.svh" 
+
+// l2_lookup.sv
+
+module l2_lookup(
+    input logic clk,
+    input logic rst, 
+    input logic lookup_mode, 
+    input logic lookup_en, 
+    input l2_way_t evict_way_buf, 
+    input var l2_tag_t tags_buf[`L2_WAYS],
+    input var state_t states_buf[`L2_WAYS],
+    line_breakdown_l2_t.in line_br, 
+    addr_breakdown_t.in addr_br, 
+
+    output logic tag_hit, 
+    output logic empty_way_found, 
+    output logic tag_hit_next, 
+    output logic empty_way_found_next, 
+    output l2_way_t way_hit,
+    output l2_way_t empty_way, 
+    output l2_way_t way_hit_next
+    );
+
+    l2_way_t empty_way_next; 
+
+    always_comb begin 
+        way_hit_next = 0;
+        tag_hit_next = 1'b0; 
+        empty_way_next = 0; 
+        empty_way_found_next = 1'b0; 
+        if (lookup_en) begin 
+            case(lookup_mode) 
+                `L2_LOOKUP : begin 
+                    for (int i = `L2_WAYS-1; i >= 0; i--) begin
+                        if (tags_buf[i] == addr_br.tag && states_buf[i] != `INVALID) begin 
+                            tag_hit_next = 1'b1; 
+                            way_hit_next = i; 
+                        end
+                        if (states_buf[i] == `INVALID) begin 
+                            empty_way_found_next = 1'b1; 
+                            empty_way_next = i; 
+                        end
+                    end
+                end
+                `L2_LOOKUP_FWD : begin 
+                    for (int i = `L2_WAYS-1; i >= 0; i--) begin 
+                        if (tags_buf[i] == line_br.tag && states_buf[i]  != `INVALID) begin 
+                            tag_hit_next = 1'b1; 
+                            way_hit_next = i;
+                        end
+                    end
+                end
+            endcase
+        end
+    end 
+    
+    always_ff @(posedge clk or negedge rst) begin 
+        if (!rst) begin 
+            way_hit <= 0;
+            tag_hit <= 1'b0; 
+            empty_way <= 0; 
+            empty_way_found <= 1'b0; 
+        end else if (lookup_en) begin 
+            way_hit <= way_hit_next;
+            tag_hit <= tag_hit_next; 
+            empty_way <= empty_way_next; 
+            empty_way_found <= empty_way_found_next;  
+        end
+    end
+
+endmodule
